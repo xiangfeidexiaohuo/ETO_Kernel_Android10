6e4b13a38c18b8eedf3a9fe2223bc018e33fdd9a
xiangfeidexiaohuo <jsjson@163.com>
2019-12-04 19:43:54 +0800

    cpufreq:others: tune default down rate limit
    Signed-off-by: YaoDao <jsjson@163.com>

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -967,7 +967,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->up_rate_limit_us =
 				cpufreq_policy_transition_delay_us(policy);
 	tunables->down_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
+				cpufreq_policy_transition_delay_us(policy) * 10;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 	tunables->iowait_boost_enable = false;
--- kernel/sched/cpufreq_schedutil_pixel.c
+++ kernel/sched/cpufreq_schedutil_pixel.c
@@ -743,7 +743,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 	tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
-	tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
+	tunables->down_rate_limit_us = LATENCY_MULTIPLIER * 10;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 	lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
